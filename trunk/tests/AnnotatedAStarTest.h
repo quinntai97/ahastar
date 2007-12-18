f0fc049e6074d28c026112253f85314e2f4baf20
dharabor <dharabor@4faa4f2f-4041-0410-a1a4-0b36eec18fc7>
2007-12-18 13:58:32 +0000

    ScenarioMananger: 	- 'terrain' and 'agentsize' now included in .scenario file 	- removed mapFlatAbstraction for generating problems; replaced with AnnotatedAStar instead. ScenarioManagerTest 	- changes to tests to reflect revised format of .scenario file AnnotatedAStarTest: 	- ? minor changes ?

--- trunk/tests/AnnotatedAStarTest.h
+++ trunk/tests/AnnotatedAStarTest.h
@@ -3,6 +3,11 @@
  *  hog
 	Tests:
 		checkPath: only visit locations that are valid given a set of terrain and size constraints
+		- Need to compare performance with A*:
+			- Require some well thought out maps; if start/goal are of the same terrain type, they must be pathable. 
+			- Run aa* and a* to see if the results are the same for single-terrain problems (no extra edges)
+			- Where multi-terrain problems must be solved, need some control over whether or not extra edges are added
+			to the map / when  (add extra edges; maybe need a removeedge function?!)
 		
  *
  *  Created by Daniel Harabor on 14/12/07.
