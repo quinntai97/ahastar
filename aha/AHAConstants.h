31938dcdafc652932d2b9c83824977096449d571
dharabor <dharabor@4faa4f2f-4041-0410-a1a4-0b36eec18fc7>
2007-12-18 13:59:47 +0000

    Collection of constants required by AHA*

---
+++ aha/AHAConstants.h
@@ -0,0 +1,26 @@
+/*
+ *  AHAConstants.h
+ *  hog
+ *
+	A collection of useful constants. 
+	AHA* is currently limited to supporting a specific subset of terrains and agentsizes.
+	These restrictions exist to make this (the initial) implementation simpler. The code can be extended to cope
+	with dynamic parameters.
+	
+ *  Created by Daniel Harabor on 19/12/07.
+ *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef AHACONSTANTS_H
+#define AHACONSTANTS_H
+
+#include "map.h"
+#include <vector>
+
+const int NUMTERRAINS = 3;
+const int NUMAGENTSIZES = 2;
+const int validterrains[NUMTERRAINS] = {kGround, kTrees, (kGround|kTrees)};
+const int agentsizes[NUMAGENTSIZES] = {1, 2};
+
+#endif
