e8030aff1e159211a49245241b8fd865d257f6a2
dharabor <dharabor@4faa4f2f-4041-0410-a1a4-0b36eec18fc7>
2007-12-18 14:03:03 +0000

    Removed duplicated constants; refers to AHAConstants.h instead

--- tests/TestHelper.h
+++ tests/TestHelper.h
@@ -11,15 +11,14 @@
 #define TESTHELPER_H
 
 #include "map.h"
+#include "AHAConstants.h"
 #include <string>
 
 using namespace std;
 
 /* consts etc */
-const int validterrains[3] = {kGround, kTrees, (kGround|kTrees)}; // terrains supported by AHA*
 const string maplocation = "/Users/dharabor/src/hog/maps/local/demo.map"; // testmap
 const string tslocation = "test.scenario"; // test scenario file
-const int agentsizes[2] = {1, 2};
 
 /* helper methods */
 string* getMessage(stringstream *ss);
