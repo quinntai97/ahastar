630b3bcbb8090cc5cb32fb0deeef676fd3ba3d97
dharabor <dharabor@4faa4f2f-4041-0410-a1a4-0b36eec18fc7>
2008-02-08 06:41:28 +0000

    devnotes:   - mindmap of various aspects of implementation; added some stuff about annotations  AnnotatedAStar: ? Shouldn't be anything changed.

---
+++ trunk/aha/mindmaps/devnotes.mm
@@ -0,0 +1,21 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1202452497354" ID="Freemind_Link_973999882" MODIFIED="1202452666081" TEXT="DevNotes">
+<node CREATED="1202452504179" FOLDED="true" ID="_" MODIFIED="1202452509435" POSITION="right" TEXT="annotations">
+<node CREATED="1202452510063" ID="Freemind_Link_1103270848" MODIFIED="1202452527263" TEXT="3 valid terrains">
+<node CREATED="1202452527869" ID="Freemind_Link_936970722" MODIFIED="1202452532728" TEXT="Ground"/>
+<node CREATED="1202452534427" ID="Freemind_Link_809883622" MODIFIED="1202452539826" TEXT="Trees"/>
+<node CREATED="1202452540615" ID="Freemind_Link_1413952293" MODIFIED="1202452543825" TEXT="Ground|Trees"/>
+</node>
+<node CREATED="1202452549881" ID="Freemind_Link_1965373593" MODIFIED="1202452560311" TEXT="Each node stores annotation for each valid terrain"/>
+<node CREATED="1202452561587" ID="Freemind_Link_562713165" MODIFIED="1202452575506" TEXT="Each node stores a terrain type">
+<node CREATED="1202452575903" ID="Freemind_Link_1790463340" MODIFIED="1202452585281" TEXT="can only set a valid terrain as the type"/>
+<node CREATED="1202452587085" ID="Freemind_Link_1708686879" MODIFIED="1202452591448" TEXT="anything else is ignored"/>
+</node>
+<node CREATED="1202452594446" ID="Freemind_Link_1869694112" MODIFIED="1202452610446" TEXT="each node stores 3 clearance values -- one for each valid terrain">
+<node CREATED="1202452610910" ID="Freemind_Link_1787656442" MODIFIED="1202452627578" TEXT="can only set a clearance value for a valid terrain type"/>
+<node CREATED="1202452629063" ID="Freemind_Link_698898863" MODIFIED="1202452633983" TEXT="anything else is ignored"/>
+</node>
+</node>
+</node>
+</map>
