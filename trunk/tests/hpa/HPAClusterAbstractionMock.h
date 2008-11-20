fa57de8e869766f630537111e7b75da86d9e8f5f
dharabor <dharabor@4faa4f2f-4041-0410-a1a4-0b36eec18fc7>
2008-11-20 14:35:58 +0000

    Added

---
+++ trunk/tests/hpa/HPAClusterAbstractionMock.h
@@ -0,0 +1,46 @@
+/*
+ *  HPAClusterAbstractionMock.h
+ *  hog
+ *
+ *  Created by dharabor on 14/11/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef HPACLUSTERABSTRACTIONMOCK_H
+#define HPACLUSTERABSTRACTIONMOCK_H
+
+#ifdef verify
+#undef verify
+#endif
+#ifdef check
+#undef check
+#endif
+#ifdef require
+#undef require
+#endif
+
+#include <mockpp/mockpp.h>
+#include <mockpp/chaining/ChainableMockObject.h> 
+#include <mockpp/chaining/CountedChainableMethod.h>
+#include <mockpp/chaining/ChainingMockObjectSupport.h>
+
+#include "HPAClusterAbstraction.h"
+
+class HPAClusterAbstractionMock : public HPAClusterAbstraction, public MOCKPP_NS::ChainableMockObject
+{
+	public:
+		HPAClusterAbstractionMock(Map* m, IClusterAStarFactory* caf, IHPAClusterFactory* cf, INodeFactory* nf, IEdgeFactory* ef, unsigned int clustersize) throw(std::invalid_argument)
+		:	HPAClusterAbstraction(m, caf, cf, nf, ef, clustersize),
+			 MOCKPP_NS::ChainableMockObject(MOCKPP_PCHAR("HPAClusterAbstractionMock"), 0)
+			//, buildClustersMocker("buildClusters", this)
+			//, getClusterMocker("getCluster", this)
+		{
+		}
+		
+		~HPAClusterAbstractionMock() 
+		{ 			
+		}
+};
+
+#endif
