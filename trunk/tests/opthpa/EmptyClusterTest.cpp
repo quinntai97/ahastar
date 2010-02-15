6c9866a3cf0eaa25f23e0b5671c2749805354214
dharabor <dharabor@4faa4f2f-4041-0410-a1a4-0b36eec18fc7>
2010-02-15 10:51:03 +0000

    added a passing test for ::adNodesToCluster; checks that parent cluster id is set for every node in the cluster area

--- trunk/tests/opthpa/EmptyClusterTest.cpp
+++ trunk/tests/opthpa/EmptyClusterTest.cpp
@@ -1,5 +1,6 @@
 #include "EmptyClusterTest.h"
 
+#include "ClusterNode.h"
 #include "ClusterNodeFactory.h"
 #include "EdgeFactory.h"
 #include "EmptyCluster.h"
@@ -37,7 +38,28 @@ void EmptyClusterTest::addNodesToClusterGrowsAMaximalSizeEmptyCluster()
 
 void EmptyClusterTest::addNodesToClusterSetsTheClusterIdOfEveryNodeInTheClusterArea()
 {
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("implement me", true, false);
+	EmptyClusterAbstraction ecmap(new Map(hpastartest.c_str()), new EmptyClusterFactory(), 
+			new ClusterNodeFactory(), new EdgeFactory());
+
+	EmptyCluster cluster(0, 0);
+	cluster.addNodesToCluster(&ecmap);
+
+	int cheight = cluster.getHeight();
+	int cwidth = cluster.getWidth();
+	int cid = cluster.getId();
+
+	for(int x=cluster.getHOrigin(); x<cluster.getHOrigin()+cwidth; x++)
+	{
+		for(int y=cluster.getVOrigin(); y<cluster.getVOrigin()+cheight; y++)
+		{
+			ClusterNode* cur = dynamic_cast<ClusterNode*>(ecmap.getNodeFromMap(x, y));
+			
+			CPPUNIT_ASSERT_EQUAL_MESSAGE("null node?!", false, cur == 0);
+			CPPUNIT_ASSERT_EQUAL_MESSAGE("node has wrong clusterId", cid, 
+					cur->getParentClusterId());
+		}
+	}
+
 }
 
 void EmptyClusterTest::addNodesToClusterFramesTheEmptyClusterWithAbstractNodes()
