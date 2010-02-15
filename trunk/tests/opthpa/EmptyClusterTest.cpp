718def153aff523f111944f0b41752f98eff5cd8
dharabor <dharabor@4faa4f2f-4041-0410-a1a4-0b36eec18fc7>
2010-02-15 10:02:53 +0000

    EmptyCluster: fixed a bug in ::expand (height not being set). + added a passing test to check cluster is maximally expanded.

--- trunk/tests/opthpa/EmptyClusterTest.cpp
+++ trunk/tests/opthpa/EmptyClusterTest.cpp
@@ -1,9 +1,12 @@
 #include "EmptyClusterTest.h"
+
+#include "ClusterNodeFactory.h"
+#include "EdgeFactory.h"
 #include "EmptyCluster.h"
 #include "EmptyClusterAbstraction.h"
-#include "EdgeFactory.h"
-#include "NodeFactory.h"
+#include "EmptyClusterFactory.h"
 #include "map.h"
+#include "TestConstants.h"
 
 CPPUNIT_TEST_SUITE_REGISTRATION( EmptyClusterTest );
 
@@ -13,13 +16,23 @@ void EmptyClusterTest::setUp()
 
 void EmptyClusterTest::tearDown()
 {
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("implement me", true, false);
 }
 
 void EmptyClusterTest::addNodesToClusterGrowsAMaximalSizeEmptyCluster()
 {
-	//EmptyClusterAbstraction ecmap(new Map(hpastartest.c_str()), 
-	
+	EmptyClusterAbstraction ecmap(new Map(hpastartest.c_str()), new EmptyClusterFactory(), 
+			new ClusterNodeFactory(), new EdgeFactory());
+
+	EmptyCluster cluster(0, 0);
+	cluster.addNodesToCluster(&ecmap);
+
+	int expectedHeight = 3;
+	int expectedWidth = 5;
+
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("cluster has wrong height", expectedHeight,
+			cluster.getHeight());
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("cluster has wrong width", expectedWidth, 
+			cluster.getWidth());
 }
 
 void EmptyClusterTest::addNodesToClusterSetsTheClusterIdOfEveryNodeInTheClusterArea()
