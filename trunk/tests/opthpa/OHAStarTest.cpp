6fd7b595ab9b83dfcfa377b283890c2698a8151e
dharabor <dharabor@4faa4f2f-4041-0410-a1a4-0b36eec18fc7>
2010-04-07 22:45:43 +0000

    Skeleton test class

---
+++ trunk/tests/opthpa/OHAStarTest.cpp
@@ -0,0 +1,23 @@
+#include "OHAStarTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION( OHAStarTest );
+
+void OHAStarTest::setUp()
+{
+}
+
+void OHAStarTest::tearDown()
+{
+}
+
+void OHAStarTest::relaxEdgeUpdatesPriorityOfNodeWithRespectToMacroParent()
+{
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("implement me", true, false);
+}
+
+void OHAStarTest::
+relaxEdgeUpdatesPriorityOfNodeAndSetsMacroParentToItselfGivenAnInterEdge()
+{
+	CPPUNIT_ASSERT_EQUAL_MESSAGE("implement me", true, false);
+}
+
